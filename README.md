e94a518f73b3c06f9a966534612533a1c27d352f
Ewunet Kassa <ewunetkassa@gmail.com>
2020-09-02 12:09:50 -0400

    generating scores

---
+++ README.md
@@ -0,0 +1,12 @@
+This application is to score a game of bowling for an individual. Given the number of pins knocked down with each roll, you will compute the total score this person has achieved.
+
+Scores entered through a terminal / console.
+Open the console and see the magic.
+
+One game of bowling consists of 10 frames.
+Each frames consisted of 2 chance to know down 10 pins, but it takes you both tries. In this case, your score for the frame is 10 plus the number of pins you knock down with you one next roll. In all other cases, your score for the frame is the number of pins you knock down.
+
+Extra rolls.
+If you bowl a strike in the last frame, you are awarded two extra rolls.
+Bowling a spare in the last time (firs two rolls add up to 10) gets you one extra roll.
+In both cases, you net score for the last frame is the total number of pins
---
+++ interface.js
@@ -0,0 +1,37 @@
+// Open the html file in this folder in a web browser window.
+// Then input in the text field a valid string of bowls.
+// Then hit the generate button to calculate the score.
+
+var genBtn = document.getElementById("generator");
+var scoreDisplay = document.getElementById("score-display");
+var gameInput = document.getElementById("game-input");
+
+// This calls the bowlScore function from bowling.js and displays it within the text span on the HTML page.
+function generate() {
+  var score = bowlScore(gameInput.value);
+  scoreDisplay.textContent = score;
+}
+
+// Functionality for clicking the gen button.
+genBtn.addEventListener("click", generate);
+
+// Functionality for enter button to execute score calculator.
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    generate();
+  }
+});
+
+gameInput.addEventListener("keydown", function (e) {
+  if (
+    !e.key.match(
+      /[0-9]|x|X|\/|-|Enter|Backspace|Delete|Control|ArrowUp|ArrowDown|ArrowLeft|ArrowRight|Ctrl|Home|End|Shift|Alt|CapsLock/
+    )
+  ) {
+    e.preventDefault();
+    gameInput.className += " red-bg";
+    setTimeout(function () {
+      gameInput.classList.remove("red-bg");
+    }, 200);
+  }
+}); // gameInput.eListen
